b65488788ee9438db6d2c674e10dd31285e6d6ba
Ravindraswamy <82256542+yravindraswamy@users.noreply.github.com>
2023-03-20 23:01:51 +0530

    GitPageVue
    component created and appended to APP

---
+++ git_practice/src/components/GitPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>this is git page</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        namge:"GitPage"
+    }
+</script>
+
+<style scoped>
+
+</style>
